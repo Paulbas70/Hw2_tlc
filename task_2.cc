a99588d7769d7915ac0385a1d60314cd069138d1
Paulbas70 <117546779+Paulbas70@users.noreply.github.com>
2022-12-13 23:54:02 +0100

    Update task_2.cc

--- task_2.cc
+++ task_2.cc
@@ -37,7 +37,9 @@
 #include "ns3/trace-source-accessor.h"
 #include "ns3/traced-value.h"
 #include "ns3/uinteger.h"
+#include <iostream>
 
+using namespace std;
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("HW2_Task_2_Team_54"); // AD-HOC MODE
@@ -94,43 +96,20 @@ main(int argc, char* argv[])
     WifiHelper wifi;
 
     WifiMacHelper macInfrastructureMod;
-    Ssid ssid = Ssid("ns-3-ssid");
+    Ssid  SSID= Ssid(ssid);
     macInfrastructureMod.SetType("ns3::StaWifiMac",
                                  "Ssid",
-                                 SsidValue(ssid),
+                                 SsidValue(SSID),
                                  "ActiveProbing",
                                  BooleanValue(false));
 
     NetDeviceContainer staDevices;
     staDevices = wifi.Install(phy, macInfrastructureMod, wifiStaNodes);
-    mac.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid));
+    macInfrastructureMod.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid));
 
     NetDeviceContainer apDevices;
     apDevices = wifi.Install(phy, macInfrastructureMod, wifiApNode);
 
-    NodeContainer p2pNodes;
-    p2pNodes.Add(wifiApNode);
-    p2pNodes.Create(1);
-    allNodes.Add(p2pNodes.Get(1));
-
-    PointToPointHelper pointToPoint;
-    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
-    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
-
-    NetDeviceContainer p2pDevices;
-    p2pDevices = pointToPoint.Install(p2pNodes);
-
-    NodeContainer csmaNodes;
-    csmaNodes.Add(p2pNodes.Get(1));
-    csmaNodes.Create(1);
-    allNodes.Add(csmaNodes.Get(1));
-
-    CsmaHelper csma;
-    csma.SetChannelAttribute("DataRate", StringValue("100Mbps"));
-    csma.SetChannelAttribute("Delay", TimeValue(NanoSeconds(6560)));
-
-    NetDeviceContainer csmaDevices;
-    csmaDevices = csma.Install(csmaNodes);
 
     // Mobility
     NS_LOG_INFO("Creazione del mobility model");
@@ -160,15 +139,7 @@ main(int argc, char* argv[])
 
     NS_LOG_INFO("Fine creazione del mobility model");
 
-    Ptr<BasicEnergySource> energySource = CreateObject<BasicEnergySource>();
-    Ptr<WifiRadioEnergyModel> energyModel = CreateObject<WifiRadioEnergyModel>();
-
-    energySource->SetInitialEnergy(300);
-    energyModel->SetEnergySource(energySource);
-    energySource->AppendDeviceEnergyModel(energyModel);
-
-    // aggregate energy source to node
-    wifiApNode.Get(0)->AggregateObject(energySource);
+ 
 
     // Install internet stack
 
@@ -192,61 +163,87 @@ main(int argc, char* argv[])
     serverApps.Stop(Seconds(10.0));
 
     UdpEchoClientHelper echoClientN3(staInterfaces.GetAddress(3), 21);
-    echoClient.SetAttribute("MaxPackets", UintegerValue(2));
-    echoClient.SetAttribute("Interval", TimeValue(Seconds(2.0)));
-    echoClient.SetAttribute("PacketSize", UintegerValue(512));
+    echoClientN3.SetAttribute("MaxPackets", UintegerValue(2));
+    echoClientN3.SetAttribute("Interval", TimeValue(Seconds(2.0)));
+    echoClientN3.SetAttribute("PacketSize", UintegerValue(512));
     ApplicationContainer clientAppsN3 = echoClientN3.Install(wifiStaNodes.Get(3)); // da controllare
-    clientApps.Start(Seconds(2.0));
-    clientApps.Stop(Seconds(4.5));
+    clientAppsN3.Start(Seconds(2.0));
+    clientAppsN3.Stop(Seconds(4.5));
 
     UdpEchoClientHelper echoClientN4(staInterfaces.GetAddress(4),21); // UDP Echo Client verso n0
-    echoClientN3.SetAttribute("MaxPackets", UintegerValue(2));
-    echoClientN3.SetAttribute("Interval", TimeValue(Seconds(3.0)));
-    echoClientN3.SetAttribute("PacketSize", UintegerValue(512));
+    echoClientN4.SetAttribute("MaxPackets", UintegerValue(2));
+    echoClientN4.SetAttribute("Interval", TimeValue(Seconds(3.0)));
+    echoClientN4.SetAttribute("PacketSize", UintegerValue(512));
     ApplicationContainer clientAppsN4 = echoClientN4.Install(wifiStaNodes.Get(4)); // UDP Echo Client installato su n3
-    cltAppN4.Start(Seconds(1.0));
-    cltAppN4.Stop(Seconds(4.5));
+    clientAppsN4.Start(Seconds(1.0));
+    clientAppsN4.Stop(Seconds(4.5));
 
     Ipv4GlobalRoutingHelper::PopulateRoutingTables();
     Simulator::Stop(Seconds(15.0));
 
     if(useRtsCts) {
-        AnimationInterface.anim("wireless-task2-rts-on.xml"); 
+        AnimationInterface anim("wireless-task2-rts-on.xml"); 
+        anim.UpdateNodeDescription(wifiStaNodes.Get(0), "SRV-0");
+        anim.UpdateNodeColor(wifiStaNodes.Get(0), 255, 0, 0);   
+
+
+
+        anim.UpdateNodeDescription(wifiStaNodes.Get(3),"CLI-3");
+        anim.UpdateNodeColor(wifiStaNodes.Get(3), 0, 255, 0);
+
+        anim.UpdateNodeDescription(wifiStaNodes.Get(4),"CLI-4");
+        anim.UpdateNodeColor(wifiStaNodes.Get(4), 0, 255, 0); 
+
+
+        anim.UpdateNodeDescription(wifiStaNodes.Get(1),"STA-1");
+        anim.UpdateNodeColor(wifiStaNodes.Get(1), 0, 0, 255);
+
+        anim.UpdateNodeDescription(wifiStaNodes.Get(2),"STA-2");
+        anim.UpdateNodeColor(wifiStaNodes.Get(2), 0, 0, 255);
+    
+    
+        anim.UpdateNodeDescription(wifiApNode.Get(0), "AP"); 
+        anim.UpdateNodeColor(wifiApNode.Get(0), 66, 49, 137); 
+
+        anim.EnablePacketMetadata(); 
+        anim.EnableWifiMacCounters(Seconds(0), Seconds(10)); 
+        anim.EnableWifiPhyCounters(Seconds(0), Seconds(10)); 
+
     }
     else {
-        AnimationInterface.anim("wireless-task2-rts-off.xml");
+        AnimationInterface anim("wireless-task2-rts-off.xml");
+        anim.UpdateNodeDescription(wifiStaNodes.Get(0), "SRV-0");
+        anim.UpdateNodeColor(wifiStaNodes.Get(0), 255, 0, 0);   
+
+
+        anim.UpdateNodeDescription(wifiStaNodes.Get(3),"CLI-3");
+        anim.UpdateNodeColor(wifiStaNodes.Get(3), 0, 255, 0);
+
+        anim.UpdateNodeDescription(wifiStaNodes.Get(4),"CLI-4");
+        anim.UpdateNodeColor(wifiStaNodes.Get(4), 0, 255, 0); 
+
+
+        anim.UpdateNodeDescription(wifiStaNodes.Get(1),"STA-1");
+        anim.UpdateNodeColor(wifiStaNodes.Get(1), 0, 0, 255);
+
+        anim.UpdateNodeDescription(wifiStaNodes.Get(2),"STA-2");
+        anim.UpdateNodeColor(wifiStaNodes.Get(2), 0, 0, 255);
+    
+    
+        anim.UpdateNodeDescription(wifiApNode.Get(0), "AP"); 
+        anim.UpdateNodeColor(wifiApNode.Get(0), 66, 49, 137); 
+
+        anim.EnablePacketMetadata(); 
+        anim.EnableWifiMacCounters(Seconds(0), Seconds(10)); 
+        anim.EnableWifiPhyCounters(Seconds(0), Seconds(10));
     }
 
-    anim.UpdateNodeDescription(wifiStaNodes.Get(0), "SRV-0");
-    anim.UpdateNodeColor(wifiStaNodes.Get(0), 255, 0, 0);   
-
-    anim.UpdateNodeDescription(wifiStaNodes.Get(3),"CLI-3");
-    anim.UpdagteNodeColor(wifiStaNodes.Get(3), 0, 255, 0);
-
-    anim.UpdateNodeDescription(wifiStaNodes.Get(4),"CLI-4");
-    anim.UpdateNodeColor(wifiStaNodes.Get(4), 0, 255, 0); 
-
-
-    anim.UpdateNodeDescription(wifiStaNodes.Get(1),"STA-1");
-    anim.UpdagteNodeColor(wifiStaNodes.Get(1), 0, 0, 255);
-
-    anim.UpdateNodeDescription(wifiStaNodes.Get(2),"STA-2");
-    anim.UpdateNodeColor(wifiStaNodes.Get(2), 0, 0, 255);
     
-    
-    anim.UpdateNodeDescription(wifiApNode.Get(0), "AP"); 
-    anim.UpdateNodeColor(wifiApNode.Get(i), 66, 49, 137); 
-
-    anim.EnablePacketMetadata(); 
-    anim.EnableWifiMacCounters(Seconds(0), Seconds(10)); 
-    anim.EnableWifiPhyCounters(Seconds(0), Seconds(10)); 
 
     if(verbose){
         LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);     //LOG abilitato per UDP SERVER (n0)
         LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);     //LOG abilitato per UDP CLIENT (n3, n4)
     }
-
-        
 
     NS_LOG_INFO("END");        //STATUS LOG INFO LEVEL
 
