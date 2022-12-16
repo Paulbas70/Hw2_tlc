7ec5e1896ed5522f94956aa5fa28e93f8a45d2e7
Paulbas70 <117546779+Paulbas70@users.noreply.github.com>
2022-12-16 16:39:30 +0100

    Update task_2.cc

--- task_2.cc
+++ task_2.cc
@@ -42,7 +42,7 @@
 using namespace std;
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE("HW2_Task_2_Team_54"); // INFRASTRUCTURE MODE
+NS_LOG_COMPONENT_DEFINE("HW2_Task_2_Team_54");      // INFRASTRUCTURE MODE
 
 int
 main(int argc, char* argv[])
@@ -71,21 +71,21 @@ main(int argc, char* argv[])
 
     NS_LOG_INFO("Creazione dei nodi e dei relativi container"); // STATUS LOG INFO LEVEL
 
-    uint32_t staNodesNum = 5;
+    uint32_t staNodesNum = 5;       // Numero di nodi sta
 
-    NodeContainer allNodes;
-    NodeContainer wifiStaNodes;
+    NodeContainer allNodes;         // Contenitore di tutti i nodi della rete WiFi Infrastructure
+    NodeContainer wifiStaNodes;       // Contenitore di tutti gli sta nodes
     wifiStaNodes.Create(staNodesNum);
     allNodes.Add(wifiStaNodes);
-    NodeContainer wifiApNode;
+    NodeContainer wifiApNode;       //Contenitore dell'AP
     wifiApNode.Create(1);
     allNodes.Add(wifiApNode);
 
-    YansWifiChannelHelper channel = YansWifiChannelHelper::Default();
-    YansWifiPhyHelper phy;
+    YansWifiChannelHelper channel = YansWifiChannelHelper::Default();   // Definizione canale di comunicazione tra i nodi
+    YansWifiPhyHelper phy;      // Definizione physical layer tra i nodi
     phy.SetChannel(channel.Create());
 
-    WifiHelper wifi;
+    WifiHelper wifi;     // Helper per creare ed installare WiFi devices
 
     WifiMacHelper macInfrastructureMod;
     Ssid  SSID= Ssid(ssid);
@@ -98,8 +98,10 @@ main(int argc, char* argv[])
     NetDeviceContainer apDevices;
     apDevices = wifi.Install(phy, macInfrastructureMod, wifiApNode);
 
+    NS_LOG_INFO("Fine creazione topologia di rete");        //STATUS LOG INFO LEVEL
 
-    // Mobility
+    ///////////////////////////////////////////////////////////////////////////////
+
     NS_LOG_INFO("Creazione del mobility model");
 
     MobilityHelper mobility;
@@ -111,7 +113,7 @@ main(int argc, char* argv[])
                                   "DeltaX",
                                   DoubleValue(5.0),
                                   "DeltaY",
-                                  DoubleValue(2.0),
+                                  DoubleValue(10.0),
                                   "GridWidth",
                                   UintegerValue(3),
                                   "LayoutType",
@@ -122,26 +124,17 @@ main(int argc, char* argv[])
     mobility.Install(wifiStaNodes);
     mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
     mobility.Install(wifiApNode);
-    // AnimationInterface::SetConstantPosition(p2pNodes.Get(1), 10, 30);
-    // AnimationInterface::SetConstantPosition(csmaNodes.Get(1), 10, 33);
 
     NS_LOG_INFO("Fine creazione del mobility model");
     
-    /*
-    energySource->SetInitialEnergy(300);
-    energyModel->SetEnergySource(energySource);
-    energySource->AppendDeviceEnergyModel(energyModel);
+    ///////////////////////////////////////////////////////////////////////////////
 
-    // aggregate energy source to node
-    wifiApNode.Get(0)->AggregateObject(energySource);
-    */
+    NS_LOG_INFO("Creazione del blocco di indirizzi IP per ogni container definito");
 
-    // Install internet stack
-
-    InternetStackHelper stack;
+    InternetStackHelper stack;      //InternetStackHelper su tutti i nodi
     stack.Install(allNodes);
 
-    // Install Ipv4 addresses
+    //------ IP Address -------------
 
     Ipv4AddressHelper address;
     address.SetBase("192.168.1.0", "255.255.255.0");
@@ -150,33 +143,47 @@ main(int argc, char* argv[])
     Ipv4InterfaceContainer apInterface;
     apInterface = address.Assign(apDevices);
 
-    // Install applications
+    NS_LOG_INFO("Fine definizione blocco di indirizzi IP");        //STATUS LOG INFO LEVEL
+
+    ///////////////////////////////////////////////////////////////////////////////
 
     NS_LOG_INFO("START");        //STATUS LOG INFO LEVEL
 
-    UdpEchoServerHelper echoServer(21);
-    ApplicationContainer serverApps = echoServer.Install(wifiStaNodes.Get(0)); // da controllare
-    serverApps.Start(Seconds(0.0));
-    serverApps.Stop(Seconds(10.0));
+    //--------------------------------------
 
-    UdpEchoClientHelper echoClientN3(staInterfaces.GetAddress(0), 21);
+    UdpEchoServerHelper echoServer(21);     // UDP Echo Server
+    ApplicationContainer serverApps = echoServer.Install(wifiStaNodes.Get(0));  // UDP Echo Server installato su n0
+    serverApps.Start(Seconds(0.0));
+    serverApps.Stop(Seconds(10.0));         //Tempo di run del server
+
+    //--------------------------------------
+
+    UdpEchoClientHelper echoClientN3(staInterfaces.GetAddress(0), 21);  // UDP Echo Client verso n0
     echoClientN3.SetAttribute("MaxPackets", UintegerValue(2));
     echoClientN3.SetAttribute("Interval", TimeValue(Seconds(2.0)));
     echoClientN3.SetAttribute("PacketSize", UintegerValue(512));
-    ApplicationContainer clientAppsN3 = echoClientN3.Install(wifiStaNodes.Get(3)); // da controllare
+    
+    ApplicationContainer clientAppsN3 = echoClientN3.Install(wifiStaNodes.Get(3)); // UDP Echo Client installato su n3
     clientAppsN3.Start(Seconds(2.0));
     clientAppsN3.Stop(Seconds(4.5));
 
-    UdpEchoClientHelper echoClientN4(staInterfaces.GetAddress(0),21); // UDP Echo Client verso n0
+    //--------------------------------------
+
+    UdpEchoClientHelper echoClientN4(staInterfaces.GetAddress(0),21);  // UDP Echo Client verso n0
     echoClientN4.SetAttribute("MaxPackets", UintegerValue(2));
     echoClientN4.SetAttribute("Interval", TimeValue(Seconds(3.0)));
     echoClientN4.SetAttribute("PacketSize", UintegerValue(512));
-    ApplicationContainer clientAppsN4 = echoClientN4.Install(wifiStaNodes.Get(4)); // UDP Echo Client installato su n3
+    
+    ApplicationContainer clientAppsN4 = echoClientN4.Install(wifiStaNodes.Get(4)); // UDP Echo Client installato su n4
     clientAppsN4.Start(Seconds(1.0));
     clientAppsN4.Stop(Seconds(4.5));
 
+    ///////////////////////////////////////////////////////////////////////////////
+
     Ipv4GlobalRoutingHelper::PopulateRoutingTables();
     Simulator::Stop(Seconds(15.0));
+
+    ///////////////////////////////////////////////////////////////////////////////
 
     if(verbose){
         LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);     //LOG abilitato per UDP SERVER (n0)
@@ -184,6 +191,8 @@ main(int argc, char* argv[])
     }
 
     NS_LOG_INFO("END");        //STATUS LOG INFO LEVEL
+
+    ///////////////////////////////////////////////////////////////////////////////
 
     if(useRtsCts) {  
         phy.EnablePcap("task2-on-n4.pcap", staDevices.Get(4), true, true);
@@ -195,72 +204,84 @@ main(int argc, char* argv[])
         phy.EnablePcap("task2-off-ap.pcap", apDevices.Get(0), true, true);         
     }
 
+    ///////////////////////////////////////////////////////////////////////////////
 
     if(useNetAnim) {
         if(useRtsCts) {
-        AnimationInterface anim("wireless-task2-rts-on.xml"); 
-        anim.UpdateNodeDescription(wifiStaNodes.Get(0), "SRV-0");
-        anim.UpdateNodeColor(wifiStaNodes.Get(0), 255, 0, 0);   
+            AnimationInterface anim ("wireless-task2-rts-on.xml"); 
+            anim.UpdateNodeDescription(wifiStaNodes.Get(0), "SRV-0");   // Nodo sta n0 SRV
+            anim.UpdateNodeColor(wifiStaNodes.Get(0), 255, 0, 0);   // R, G, B
 
+            anim.UpdateNodeDescription(wifiStaNodes.Get(1),"STA-1");    // Nodo sta n1 
+            anim.UpdateNodeColor(wifiStaNodes.Get(1), 0, 0, 255);   // R, G, B
 
+            anim.UpdateNodeDescription(wifiStaNodes.Get(2),"STA-2");    // Nodo sta n2
+            anim.UpdateNodeColor(wifiStaNodes.Get(2), 0, 0, 255);   // R, G, B
 
-        anim.UpdateNodeDescription(wifiStaNodes.Get(3),"CLI-3");
-        anim.UpdateNodeColor(wifiStaNodes.Get(3), 0, 255, 0);
+            anim.UpdateNodeDescription(wifiStaNodes.Get(3),"CLI-3");    // Nodo sta client n3
+            anim.UpdateNodeColor(wifiStaNodes.Get(3), 0, 255, 0);   // R, G, B
 
-        anim.UpdateNodeDescription(wifiStaNodes.Get(4),"CLI-4");
-        anim.UpdateNodeColor(wifiStaNodes.Get(4), 0, 255, 0); 
+            anim.UpdateNodeDescription(wifiStaNodes.Get(4),"CLI-4");    // Nodo sta client n4
+            anim.UpdateNodeColor(wifiStaNodes.Get(4), 0, 255, 0);   // R, G, B
 
-
-        anim.UpdateNodeDescription(wifiStaNodes.Get(1),"STA-1");
-        anim.UpdateNodeColor(wifiStaNodes.Get(1), 0, 0, 255);
-
-        anim.UpdateNodeDescription(wifiStaNodes.Get(2),"STA-2");
-        anim.UpdateNodeColor(wifiStaNodes.Get(2), 0, 0, 255);
+       
     
+            anim.UpdateNodeDescription(wifiApNode.Get(0), "AP");    //Nodo AP
+            anim.UpdateNodeColor(wifiApNode.Get(0), 66, 49, 137);   // R, G, B
+
+            anim.EnablePacketMetadata();    // Packet Metadata
+            anim.EnableWifiMacCounters(Seconds(0), Seconds(10));    // Tracing MAC
+            anim.EnableWifiPhyCounters(Seconds(0), Seconds(10));    // Tracing PHY
+
+            Simulator::Stop(Seconds(12.0));
+
+            Simulator::Run();
+            Simulator::Destroy();
+
+            return 0;
+            }
+        else {
+            AnimationInterface anim ("wireless-task2-rts-off.xml");
+            anim.UpdateNodeDescription(wifiStaNodes.Get(0), "SRV-0");   // Nodo sta n0 SRV
+            anim.UpdateNodeColor(wifiStaNodes.Get(0), 255, 0, 0);  // R, G, B 
+
+            anim.UpdateNodeDescription(wifiStaNodes.Get(1),"STA-1");    // Nodo sta n1 
+            anim.UpdateNodeColor(wifiStaNodes.Get(1), 0, 0, 255);   // R, G, B
+
+            anim.UpdateNodeDescription(wifiStaNodes.Get(2),"STA-2");    // Nodo sta n2
+            anim.UpdateNodeColor(wifiStaNodes.Get(2), 0, 0, 255);   // R, G, B
+
+            anim.UpdateNodeDescription(wifiStaNodes.Get(3),"CLI-3");    // Nodo sta client n3
+            anim.UpdateNodeColor(wifiStaNodes.Get(3), 0, 255, 0);   // R, G, B
+
+            anim.UpdateNodeDescription(wifiStaNodes.Get(4),"CLI-4");    // Nodo sta client n4
+            anim.UpdateNodeColor(wifiStaNodes.Get(4), 0, 255, 0);   // R, G, B
+
     
-        anim.UpdateNodeDescription(wifiApNode.Get(0), "AP"); 
-        anim.UpdateNodeColor(wifiApNode.Get(0), 66, 49, 137); 
+            anim.UpdateNodeDescription(wifiApNode.Get(0), "AP");    //Nodo AP
+            anim.UpdateNodeColor(wifiApNode.Get(0), 66, 49, 137);   // R, G, B
 
-        anim.EnablePacketMetadata(); 
-        anim.EnableWifiMacCounters(Seconds(0), Seconds(10)); 
-        anim.EnableWifiPhyCounters(Seconds(0), Seconds(10)); 
+            anim.EnablePacketMetadata();    // Packet Metadata
+            anim.EnableWifiMacCounters(Seconds(0), Seconds(10));    // Tracing MAC
+            anim.EnableWifiPhyCounters(Seconds(0), Seconds(10));    // Tracing PHY
 
-    }
-    else {
-        AnimationInterface anim("wireless-task2-rts-off.xml");
-        anim.UpdateNodeDescription(wifiStaNodes.Get(0), "SRV-0");
-        anim.UpdateNodeColor(wifiStaNodes.Get(0), 255, 0, 0);   
+            Simulator::Stop(Seconds(12.0));
 
+            Simulator::Run();
+            Simulator::Destroy();
 
-        anim.UpdateNodeDescription(wifiStaNodes.Get(3),"CLI-3");
-        anim.UpdateNodeColor(wifiStaNodes.Get(3), 0, 255, 0);
-
-        anim.UpdateNodeDescription(wifiStaNodes.Get(4),"CLI-4");
-        anim.UpdateNodeColor(wifiStaNodes.Get(4), 0, 255, 0); 
-
-
-        anim.UpdateNodeDescription(wifiStaNodes.Get(1),"STA-1");
-        anim.UpdateNodeColor(wifiStaNodes.Get(1), 0, 0, 255);
-
-        anim.UpdateNodeDescription(wifiStaNodes.Get(2),"STA-2");
-        anim.UpdateNodeColor(wifiStaNodes.Get(2), 0, 0, 255);
-    
-    
-        anim.UpdateNodeDescription(wifiApNode.Get(0), "AP"); 
-        anim.UpdateNodeColor(wifiApNode.Get(0), 66, 49, 137); 
-
-        anim.EnablePacketMetadata(); 
-        anim.EnableWifiMacCounters(Seconds(0), Seconds(10)); 
-        anim.EnableWifiPhyCounters(Seconds(0), Seconds(10));
-    }
-    }
+            return 0;
+            }
+        }
    
+    else{
 
-    ///////////////////////////////////////////////////////////////////////////////
+        Simulator::Stop(Seconds(12.0));
 
+        Simulator::Run();
+        Simulator::Destroy();
 
-
-    Simulator::Run();
-    Simulator::Destroy();
-    return 0;
+        return 0;
+        
+    }   
 }
